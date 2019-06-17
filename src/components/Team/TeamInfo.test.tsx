a6bae4daf9f37eacb321a97c10cab01d340d509d
Tommi Soikkeli <soikkelitommi@gmail.com>
2019-06-17 15:58:54 +0300

    more teaminfo tests

--- src/components/Team/TeamInfo.test.tsx
+++ src/components/Team/TeamInfo.test.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { TeamInfo } from './TeamInfo';
+import { ITeam } from '../../models/team';
 
-const testTeam1 = {
+const testTeam1: ITeam = {
   id: 1,
   name: 'FC Test',
   crestUrl: 'cresturl',
@@ -19,28 +20,28 @@ const testTeam1 = {
       shirtNumber: 1
     },
     {
-      id: 11,
+      id: 12,
       name: 'Test Defender',
       position: 'Defender',
       role: 'PLAYER',
       shirtNumber: 2
     },
     {
-      id: 11,
+      id: 13,
       name: 'Test Midfielder',
       position: 'Midfielder',
       role: 'PLAYER',
       shirtNumber: 4
     },
     {
-      id: 11,
-      name: 'Test Attacker',
+      id: 14,
+      name: 'Lionel Messi',
       position: 'Attacker',
       role: 'PLAYER',
       shirtNumber: 10
     },
     {
-      id: 11,
+      id: 15,
       name: 'Test Coach',
       position: '',
       role: 'COACH',
@@ -49,7 +50,18 @@ const testTeam1 = {
   ]
 };
 
-describe('TeamInfo', () => {
+const testTeam2: ITeam = {
+  id: 2,
+  name: 'FC Test',
+  crestUrl: 'cresturl',
+  tla: 'FCT',
+  founded: 2019,
+  venue: 'Test Park',
+  clubColors: 'Black / Red',
+  squad: []
+};
+
+describe('TeamInfo with squad', () => {
   let wrapper: any;
 
   beforeEach(() => {
@@ -75,10 +87,32 @@ describe('TeamInfo', () => {
   });
 
   it('renders squad correctly', () => {
+    expect(wrapper.find('.squad').length).toBe(1);
     const players = wrapper.find('.position-divider');
     expect(players.length).toBe(4);
     players.forEach((p: any, i: number) => {
       expect(p.text()).toContain(testTeam1.squad[i].name);
     });
   });
+
+  it('adds goat when conditions are met', () => {
+    const goat = '🐐';
+    expect(wrapper.find('.player-info').at(3).text()).toContain(goat);
+  });
+});
+
+describe('TeamInfo without squad', () => {
+  let wrapper: any;
+
+  beforeEach(() => {
+    wrapper = shallow(<TeamInfo team={testTeam2} />);
+  });
+
+  it('does not render coach', () => {
+    expect(wrapper.find('.info-row').length).toBe(3);
+  });
+
+  it('does not render players', () => {
+    expect(wrapper.find('.squad').length).toBe(0);
+  })
 });
