985615cd4dafa80f0e4342ab6996213d9c96d214
Tommi Soikkeli <soikkelitommi@gmail.com>
2019-06-20 20:55:46 +0300

    colors, css fixes

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,5 +2,7 @@ import * as React from 'react';
 import { BeatLoader } from 'react-spinners';
 
 export const Loading: React.FC = () => (
-  <BeatLoader sizeUnit='px' size={20} color={'#a5a1a1'} />
+  <div style={{ marginTop: 100 }}>
+    <BeatLoader sizeUnit='px' size={20} color={'#a5a1a1'} />
+  </div>
 );
