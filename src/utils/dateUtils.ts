20f29a9a2770cbbc3f1b2df38fd3a8eefae0a844
Tommi Soikkeli <soikkelitommi@gmail.com>
2019-08-13 11:55:08 +0300

    removed unnecessary template literals

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,11 +1,11 @@
 import { DateTime } from 'luxon';
 
 export const parseDate = (date: string): string =>
-  `${DateTime.fromISO(date).toLocaleString()}`;
+  DateTime.fromISO(date).toLocaleString();
 
 export const parseTime = (date: string): string =>
-  `${DateTime.fromISO(date).toLocaleString({
+  DateTime.fromISO(date).toLocaleString({
     hour: '2-digit',
     minute: '2-digit',
     hour12: false
-  })}`;
+  });
